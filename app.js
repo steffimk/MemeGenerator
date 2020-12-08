6bc6910bf1baa1cb30d1795d3e197767cb56dc37
Dominik Grenz <dominik.grenz@campus.lmu.de>
2020-12-08 15:02:13 +0100

    Fix semicolon

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/memes', memesRouter)
+app.use('/memes', memesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
