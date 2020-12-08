6cef5042a0a5c62546c296433c2e139a7da7da75
Dominik Grenz <dominik.grenz@campus.lmu.de>
2020-12-08 15:14:45 +0100

    Add some memes

--- routes/memes.js
+++ routes/memes.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+const memes = [
+    {
+        "id": "181913649",
+        "name": "Drake Hotline Bling",
+        "url": "https://i.imgflip.com/30b1gx.jpg",
+        "width": 1200,
+        "height": 1200,
+        "box_count": 2
+    },
+    {
+        "id": "112126428",
+        "name": "Distracted Boyfriend",
+        "url": "https://i.imgflip.com/1ur9b0.jpg",
+        "width": 1200,
+        "height": 800,
+        "box_count": 3
+    },
+]
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.json({
+        "success": true,
+        "data": {"memes": memes}
+    })
+});
+
+router.post('/', function(req, res, next){
+
 });
 
 module.exports = router;
