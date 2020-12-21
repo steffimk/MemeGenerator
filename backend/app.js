f43d978868c2cfeaa80e949c1f37afa82c93e102
Dominik Grenz <dominik.grenz@campus.lmu.de>
2020-12-21 21:30:22 +0100

    Remove auth and fix db connection

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-const db = require('monk')('localhost/ommOfficialDB');
+var db = require('monk')('mongo:27017/ommOfficialDB');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
