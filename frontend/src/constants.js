179ea7d77891fd763ff17396fdba81eeaa170350
Steffi <stefanie.m.kloss@t-online.de>
2021-02-23 14:48:20 +0100

    updated gallery url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -9,5 +9,5 @@ export const LS_USERNAME = 'memeGen_username'
 export const LS_JWT = 'memeGen_jwt'
 
 export const FRONTEND_URL = 'http://localhost:3000'
-export const GALLERY_URL = FRONTEND_URL + '/gallery'
+export const GALLERY_URL = FRONTEND_URL + '/'
 export const MEME_URL = FRONTEND_URL + '/meme'
